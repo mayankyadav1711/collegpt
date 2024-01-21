003c0b7e18c6fcfcdeb86b5db9e8720828ff843a
mayankyadav1711 <mykyadav2003@gmail.com>
2024-01-21 23:57:30 +0530

    aeinvayi

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express();
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 const { MONGOURI } = require('./config/keys')
+//cors policy 
 app.use(cors (
   {
      origin: ["https://collegpt.vercel.app"],
