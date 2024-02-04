7935c208f06f29047b54ebd712b8338afda9e10b
Mayank Yadav <114771432+mayankyadav1711@users.noreply.github.com>
2024-02-04 17:49:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,14 +6,16 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 const { MONGOURI } = require('./config/keys')
 //cors policy 
-app.use(cors (
-  {
-    origin: ["https://www.collegpt.com/",  "https://collegpt.com/","http://localhost:3000", "https://collegpt.vercel.app/"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-     credentials: true
-
-  }
-));
+app.use(cors({
+  origin: [
+    "https://www.collegpt.com/",
+    "https://collegpt.com/",
+    "http://localhost:3000",
+    "https://collegpt.vercel.app/"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 mongoose.connect(MONGOURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
