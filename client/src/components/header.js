946f515d55d6f3d783b08475349bae5cf385e7c9
Mayank Yadav <114771432+mayankyadav1711@users.noreply.github.com>
2024-02-22 22:52:33 +0530

    Update header.js

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -8,7 +8,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import "react-toastify/dist/ReactToastify.css";
 import defaultprofilepic from "./images/60111.png";
-import logo from "./images/ColleGPT_Logo-01.svg";
+import logo from "./images/collegpt-pink.svg";
 import Welcome_Collegpt from "./collegptanimation";
 
 
@@ -42,38 +42,6 @@ const Header = () => {
     }
   }, [state]);
 
-
-  const defaultProfile = {
-    profilePic: defaultprofilepic,
-    name: "user",
-    Roles: ["Student"],
-  };
-
-  if (!state?.user) {
-    return (
-      <header className="header headerimp">
-        <section className="flex">
-          <Link to="/">
-            <a className=" logo-trans">
-              <img className="  logo-header" src={logo} alt="College GPT" />
-              College GPT
-            </a>
-          </Link>
-
-          <Link to="/">
-            <Welcome_Collegpt />
-          </Link>
-
-          <div className="icons">
-            <div id="user-btn" className="fas fa-users"></div>
-            <div id="toggle-btn" className="fas fa-moon"></div>
-          </div>
-
-         
-        </section>
-      </header>
-    );
-  }
   return (
     
     <header className="header headerimp" >
@@ -108,47 +76,58 @@ const Header = () => {
           <div id="toggle-btn" className="fas fa-moon"></div>
         </div>
 
-    
-        <div className="profile">
-          <img src={userProfile?.profilePic || defaultProfile.profilePic} className="image" alt="" />
-          <h3 className="name">{userProfile?.name || defaultProfile.name}</h3>
-          <p className="role">{userProfile?.Roles[0] || defaultProfile.Roles[0]}</p>
-          {state?.user && (
+        {isLoading ? (
+          <div className="profile">
+            <div className="image"></div>
+            <FontAwesomeIcon
+              icon={faSpinner}
+              spin
+              size="2x"
+              className="white-icon"
+            />
+          </div>
+        ) : (
+          <div className="profile">
+            <img
+              src={userProfile?.profilePic || defaultprofilepic}
+              className="image"
+              alt=""
+            />
+            <h3 className="name">{userProfile?.name}</h3>
+            <p className="role">{userProfile?.Roles[0]}</p>
             <Link to="/updateProfile">
-              <a className="inline-btn">View Profile</a>
+              <a className="inline-btn">view profile</a>
             </Link>
-          )}
-          {state?.user && (
             <div className="flex-btn">
+              <Link to="/login">
               <button
-                onClick={() => {
-                  localStorage.clear();
-                  dispatch({ type: "CLEAR" });
-                  toast.success("Logout Successfully!!");
-                  navigate("/login");
-                }}
-              >
-                <a className="inline-btn" style={{ marginLeft: "0rem" }}>
-                  Logout
-                </a>
-              </button>
-              <Link to="/userlist">
-                <a
-                  className="inline-btn"
-                  style={{
-                    width: "46%",
-                    marginLeft: "1.5rem",
-                    paddingLeft: "1.5rem",
-                    paddingRight: "6rem",
-                    textAlign: "center",
-                  }}
-                >
-                  Community
-                </a>
+  onClick={() => {
+    localStorage.clear();
+    dispatch({ type: "CLEAR" });
+    toast.success("Logout Successfully!!");
+    navigate("/login");
+  }}
+>
+  <a className="inline-btn"style={{
+   marginLeft:"0rem"
+  }}>Logout</a>
+
+</button>
+
+  <Link to="/userlist">
+  <a className="inline-btn" style={{
+    width: "46%",marginLeft:"1.5rem", paddingLeft:'1.5rem',paddingRight:'6rem',textAlign:"center"
+  }}>Community</a>
+  </Link>
+
+
+
               </Link>
             </div>
-          )}
-        </div>
+          </div>
+        )}
+
+    
       </section>
     </header>
   );
