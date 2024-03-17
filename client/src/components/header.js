4b32aa1a71cc05e98daf9103373df6f6178c19ec
KauraniDivya <kauranidivya@gmail.com>
2024-03-18 00:04:27 +0530

    cross

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../App";
 import toast from 'react-hot-toast';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinner, faBars, faAngleDown } from "@fortawesome/free-solid-svg-icons"; // Added faAngleDown for dropdown indicator
+import { faSpinner, faBars, faAngleDown, faTimes } from "@fortawesome/free-solid-svg-icons"; // Added faAngleDown for dropdown indicator
 import defaultprofilepic from "./images/60111.png";
 import Welcome_Collegpt from "./collegptanimation";
 
@@ -17,6 +17,29 @@ const Header = () => {
   const menuRef = useRef(null);
   const profileRef = useRef(null);
 
+  useEffect(() => {
+
+    
+    if (state && state._id) {
+      setIsLoading(true);
+
+      fetch(`https://api-collegpt.vercel.app/view-profile/${state?._id}`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setUserProfile(data);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          setIsLoading(false);
+        });
+    }
+  }, [state]);
   useEffect(() => {
     // Function to close the menu and profile dropdown when clicking anywhere outside
     function handleClickOutside(event) {
@@ -49,6 +72,11 @@ const Header = () => {
     setIsProfileOpen(false); // Close the profile dropdown when menu is opened
   };
 
+  // Function to close the mobile menu
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <header className="header">
       <div className="container mx-auto flex items-center justify-between py-4">
@@ -57,53 +85,37 @@ const Header = () => {
         </Link>
         {/* Desktop Menu  */}
         <nav className="hidden md:flex items-center space-x-20 ml-auto">
+          <div className="text-gray-900 dark:text-white text-3xl px-3 hover:text-blue-900 dark:hover:text-blue-700">
+            <Link to="/">Home</Link>
+          </div>
+          <div className="text-gray-900 dark:text-white text-3xl px-3 hover:text-blue-900 dark:hover:text-blue-700">
+            <Link to="/courses">X-Notes</Link>
+          </div>
+          <div className="text-gray-900 dark:text-white text-3xl px-3 hover:text-blue-900 dark:hover:text-blue-700">
+            <Link to="/userlist">Community</Link>
+          </div>
+          <div className="text-gray-900 dark:text-white text-3xl px-3 hover:text-blue-900 dark:hover:text-blue-700">
+            <Link to="/about">About</Link>
+          </div>
+          <div className="text-gray-900 dark:text-white text-3xl px-3 hover:text-blue-900 dark:hover:text-blue-700">
+            <Link to="/contributor_form">Contribute</Link>
+          </div>
           <Link
-            to="/"
+            to="/contact"
             className="text-gray-900 font-medium dark:text-white text-3xl px-3 hover:text-blue-900 dark:hover:text-blue-700"
           >
-            Home
-          </Link>
-          <Link
-            to="/about"
-            className="text-gray-900 font-medium dark:text-white text-3xl px-3 hover:text-blue-900 dark:hover:text-blue-700"
-          >
-            About
-          </Link>
-          <Link
-            to="/userlist"
-            className="text-gray-900 font-medium dark:text-white text-3xl px-3 hover:text-blue-900 dark:hover:text-blue-700"
-          >
-            Community
-          </Link>
-          <Link
-            to="/"
-            className="text-gray-900 font-medium dark:text-white text-3xl px-3 hover:text-blue-900 dark:hover:text-blue-700"
-          >
-            Cheatsheets
-          </Link>
-          <Link
-            to="/"
-            className="text-gray-900 font-medium dark:text-white text-3xl px-3 hover:text-blue-900 dark:hover:text-blue-700"
-          >
-            Roadmaps
-          </Link>
-          <Link
-            to="/courses"
-            className="text-gray-900 font-medium dark:text-white text-3xl px-3 hover:text-blue-900 dark:hover:text-blue-700"
-          >
-            Notes
+            Contact
           </Link>
         </nav>
 
         <div className="flex items-center space-x-0 mr-0 ml-auto">
           <div className="icons">
-            {/* <div id="user-btn" className="fas fa-users"></div> */}
             <div id="toggle-btn" className="fas fa-moon"></div>
           </div>
           <div className="relative" ref={profileRef}>
-            <img
-              src={defaultprofilepic}
-              className="w-16 h-16 ml-3 cursor-pointer"
+          <img
+               src={userProfile?.profilePic || defaultprofilepic}
+              className="w-16 h-16 ml-3 cursor-pointer rounded-full"
               onClick={handleProfileClick} // Attach click handler for profile picture
               alt="Profile Picture"
             />
@@ -115,7 +127,6 @@ const Header = () => {
               ref={menuRef}
             >
               <div className="py-1">
-                {/* Logout option for desktop*/}
                 <button
                   className="block w-full px-4 py-2 text-2xl text-white hover:bg-gray-100 hover:text-black"
                   onClick={() => {
@@ -127,7 +138,6 @@ const Header = () => {
                 >
                   Logout
                 </button>
-                {/* View Profile option */}
                 <Link
                   to="/updateProfile"
                   className="block w-full text-center px-4 py-2 text-2xl text-white hover:bg-gray-100 hover:text-black"
@@ -141,40 +151,9 @@ const Header = () => {
             icon={faAngleDown}
             className={`h-6 w-6 ${isProfileOpen ? "transform rotate-180" : ""}`}
           />
-          {isLoading ? (
-            <div className="profile">
-              <div className="image"></div>
-              <FontAwesomeIcon
-                icon={faSpinner}
-                spin
-                size="2x"
-                className="white-icon"
-              />
-            </div>
-          ) : (
-            <div className="profile flex items-center space-x-4">
-              <img
-                src={userProfile?.profilePic || defaultprofilepic}
-                className="w-10 h-10 rounded-full"
-                alt=""
-              />
-              <div className="text-gray-800 dark:text-gray-300">
-                <h3 className="font-medium">{userProfile?.name}</h3>
-                <p>{userProfile?.Roles[0]}</p>
-              </div>
-              <div className="flex items-center ">
-                <Link to="/updateProfile" className="inline-btn">
-                  View Profile
-                </Link>
-            
-                <Link to="/userlist" className="inline-btn">
-                  Community
-                </Link>
-              </div>
-            </div>
-          )}
         </div>
-          {/* Hamburger icon for menu  */}
+
+        {/* Hamburger icon for menu  */}
         <div className="md:hidden">
           <FontAwesomeIcon
             icon={faBars}
@@ -192,25 +171,12 @@ const Header = () => {
         }`}
       >
         <button
-          onClick={toggleMenu}
+          onClick={closeMenu}
           type="button"
           className="absolute top-4 right-4 w-10 h-10 text-gray-700 dark:text-gray-300 rounded-md hover:bg-gray-200 dark:hover:bg-gray-700 flex items-center justify-center"
           aria-label="Close Menu"
         >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            ></path>
-          </svg>
+          <FontAwesomeIcon icon={faTimes} className="w-12 h-12" />
         </button>
 
         {/* Menu items for Mobile*/}
@@ -219,10 +185,22 @@ const Header = () => {
           className="flex flex-col items-center justify-center min-h-full space-y-40 w-full backdrop-blur-3xl bg-opacity-50"
         >
           <Link
-            to="#"
+            to="/"
             className="py-4 text-5xl text-white dark:text-white hover:text-blue-900 dark:hover:text-blue-700"
           >
             Home
+          </Link>
+          <Link
+            to="/courses"
+            className="py-4 text-5xl text-white dark:text-white hover:text-blue-900 dark:hover:text-blue-700"
+          >
+            X-Notes
+          </Link>
+          <Link
+            to="/userlist"
+            className="py-4 text-5xl text-white dark:text-white hover:text-blue-900 dark:hover:text-blue-700"
+          >
+            Community
           </Link>
           <Link
             to="/about"
@@ -230,29 +208,18 @@ const Header = () => {
           >
             About
           </Link>
+         
           <Link
-            to="#"
+            to="/contributor_form"
             className="py-4 text-5xl text-white dark:text-white hover:text-blue-900 dark:hover:text-blue-700"
           >
-            Community
+            Contribute
           </Link>
           <Link
-            to="#"
+            to="/contact"
             className="py-4 text-5xl text-white dark:text-white hover:text-blue-900 dark:hover:text-blue-700"
           >
-            Cheatsheets
-          </Link>
-          <Link
-            to="#"
-            className="py-4 text-5xl text-white dark:text-white hover:text-blue-900 dark:hover:text-blue-700"
-          >
-            Roadmaps
-          </Link>
-          <Link
-            to="#"
-            className="py-4 text-5xl text-white dark:text-white hover:text-blue-900 dark:hover:text-blue-700"
-          >
-            Notes
+            Contact
           </Link>
         </nav>
       </div>
