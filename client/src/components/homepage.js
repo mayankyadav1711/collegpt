2ad7ac321739366c30fa5321ec9066bd1f19e830
KauraniDivya <kauranidivya@gmail.com>
2024-03-18 17:49:21 +0530

    updated hero for phone

--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -55,64 +55,34 @@ const Homepage = () => {
     return (
         <>
             <main className="mt-8 overflow-hidden text-gray-900 dark:text-white ">
-            <section class="home-grid">
-                    <section>
-                        <div class="py-14 px-4 mx-auto max-w-screen-xl text-center lg:py-52 lg:px-12 mb-16  ">
-                            <a
-                                href="#"
-                                onClick={scrollToNextSection}
-                                class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-12 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
-                                role="alert">
-                                <span class="text-2xl bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">
-                                    New
-                                </span>{" "}
-                                <span class="text-2xl font-medium">
-                                    ColleGPT is Live! 🚀
-                                </span>
-                                <svg
-                                    class="ml-2 w-5 h-5"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        fill-rule="evenodd"
-                                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                        clip-rule="evenodd"></path>
-                                </svg>
-                            </a>
-                            <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-6xl lg:text-9xl dark:text-white">
-                                Get Prepared Together
-                            </h1>
-                            <p class="mb-16 text-lg font-normal text-gray-500 lg:text-3xl sm:px-16 xl:px-48 dark:text-gray-400">
-                                With the intention of learning and growing
-                                together, our team has built this platform, to
-                                revolutionize your academic journey by providing
-                                engaging resources and supportive community.
-                            </p>
-                            <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-                              
-                                    {" "}
-                                    <a
-                                        href="#"
-                                        onClick={scrollToNextSection}
-                                        class="text-2xl inline-flex justify-center items-center py-3 px-5  font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                                        Explore more
-                                        <svg
-                                            class="ml-2 -mr-1 w-5 h-5"
-                                            fill="currentColor"
-                                            viewBox="0 0 20 20"
-                                            xmlns="http://www.w3.org/2000/svg">
-                                            <path
-                                                fill-rule="evenodd"
-                                                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                                clip-rule="evenodd"></path>
-                                        </svg>
-                                    </a>
-                          
-                            </div>
-                        </div>
-                    </section>
-                </section>
+            <section class="home-grid h-screen">
+    <section class="flex items-center justify-center h-full">
+        <div class="py-14 px-4 mx-auto max-w-screen-xl text-center lg:py-52 lg:px-12 mb-16">
+            <a href="#" onClick={scrollToNextSection} class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-12 text-xl lg:text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
+                <span class="text-4xl lg:text-2xl bg-primary-600 rounded-full text-white px-8 lg:px-4 py-3.5 lg:py-1.5 mr-3">New</span>
+                <span class="text-4xl lg:text-2xl font-medium">ColleGPT is Live! 🚀</span>
+                <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+                </svg>
+            </a>
+            <h1 class="mb-4 text-8xl font-extrabold tracking-tight leading-none text-gray-900 md:text-6xl lg:text-9xl dark:text-white">
+                Get Prepared Together
+            </h1>
+            <p class="mb-16 text-5xl font-normal text-gray-500 lg:text-3xl sm:px-16 xl:px-48 dark:text-gray-400">
+                With the intention of learning and growing together, our team has built this platform, to revolutionize your academic journey by providing engaging resources and supportive community.
+            </p>
+            <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+                <a href="#" onClick={scrollToNextSection} class="text-5xl lg:text-2xl inline-flex justify-center items-center py-3 px-5 font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                    Explore more
+                    <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                    </svg>
+                </a>
+            </div>
+        </div>
+    </section>
+</section>
+
                 <section id="feature-section-lg" className="hidden lg:block">
                 <div className="flex justify-center">
                 <div className="mt-16 pt-16 max-w-screen-md lg:mt-16">
@@ -163,7 +133,7 @@ const Homepage = () => {
       </div>
     </section>
     <section id="feature-section-sm" className="block lg:hidden relative">
-    <div className="my-8 max-w-screen-md  flex justify-center mt-32">
+    <div className="my-8 max-w-screen-md pt-40 flex justify-center mt-32">
         <h2 className="text-6xl text-gray-950 dark:text-white font-semibold">
         Experience ColleGPT 🚀 <br />
                                 </h2>
