61d03b95d478741a344c8a664bcc3147d9315fe6
Mayank Yadav <114771432+mayankyadav1711@users.noreply.github.com>
2024-05-11 16:21:00 +0530

    Update keys.js

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,7 +1,4 @@
-// module.exports = {
-//     MONGOURI: "mongodb+srv://database_user:database_user123@cluster0.0xzovmi.mongodb.net/",
-//     JWT_SECRET:"shfgiahoiauhroiuha"
-// }
+
 
 if(process.env.NODE_ENV==='production'){
     module.exports = require('./prod')
